8bedcf0a0ae1007ae5824c2794460289c1704595
Yasser-Aboibrahim <yasser.fayez.aboibrahim@gmail.com>
2023-11-15 08:21:38 +0200

    fix errors

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
-import { RestaurantsScreen } from "./src/featsures/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
+
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -21,12 +23,13 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <RestaurantsScreen />
-        <ExpoStatusBar style="auto" />
       </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { SafeAreaView, StatusBar } from "react-native";
-// import { colors } from "../../../infrastructure/theme/colors";
 import { Searchbar } from "react-native-paper";
-import { RestaurantInfoCard } from "../components/restauratnt-info-Card";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 import styled from "styled-components/native";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`}
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
