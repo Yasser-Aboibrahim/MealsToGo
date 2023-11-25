e068cba09c1e50646d10a940cca2f22a869e6e35
Yasser-Aboibrahim <yasser.fayez.aboibrahim@gmail.com>
2023-11-25 18:23:43 +0200

    update spacer

--- src/featsures/restaurants/components/restauratnt-info-Card.js
+++ src/featsures/restaurants/components/restauratnt-info-Card.js
@@ -1,70 +1,65 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
-import { Card } from "react-native-paper";
-import { colors } from "../../../infrastructure/theme/colors";
 import styled from "styled-components/native";
+import { Text, Image, View } from "react-native";
+import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+import { Spacer }  from "../../../components/spacer/spacer.component"
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
-const Title = styled.Text`
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`
   font-family: ${(props) => props.theme.fonts.heading};
   font-size: ${(props) => props.theme.fontSizes.body};
-  background-color: ${(props) => props.theme.colors.bg.secondary};
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body};
-  ont-size: ${(props) => props.theme.fontSizes.caption};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.secondary};
-`;
-
-const RestaurantCard = styled(Card)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.secondary};
-`;
-
-const Row = styled.View`
+const Rating = styled.View`
   flex-direction: row;
-  padding-top: ${(props) => props.theme.space[3]};
-  padding-buttom: ${(props) => props.theme.space[3]};
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
-
-// const open = styled(SvgXml)`
-//   flex-direction: row;
-// `;
-
 const Section = styled.View`
   flex-direction: row;
   align-items: center;
 `;
-
 const SectionEnd = styled.View`
   flex: 1;
   flex-direction: row;
   justify-content: flex-end;
 `;
 
-export const RestaurantInfoCard = ({ retaurant = {} }) => {
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "retaurant",
+    name = "Some Restaurant",
     icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
-    photos = ["https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"],
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
     address = "100 some random street",
     isOpenNow = true,
     rating = 4,
     isClosedTemporarily = true,
-  } = retaurant;
+  } = restaurant;
 
-  const ratingArray = Array.from(new Array(Math.ceil(rating)));
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
@@ -72,23 +67,24 @@ export const RestaurantInfoCard = ({ retaurant = {} }) => {
       <Info>
         <Title>{name}</Title>
         <Section>
-          <Row>
+          <Rating>
             {ratingArray.map(() => (
               <SvgXml xml={star} width={20} height={20} />
             ))}
-            <SectionEnd>
-              <View style={{ paddingLeft: 16 }} />
-              {isClosedTemporarily && (
-                <Text variant="label" style={{ color: "red" }}>
-                  CLOSED TEMPORARILY
-                </Text>
-              )}
-              <View style={{ paddingLeft: 6 }} />
-              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-              <View style={{ paddingLeft: 6 }} />
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
-            </SectionEnd>
-          </Row>
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+            <Spacer position="left" size="large">
+                {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+                <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
         </Section>
         <Address>{address}</Address>
       </Info>
