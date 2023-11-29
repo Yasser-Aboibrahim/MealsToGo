af75f6b2568a8494b841795d296d873559e45f26
Yasser-Aboibrahim <yasser.fayez.aboibrahim@gmail.com>
2023-11-29 09:13:25 +0200

    styles enhancement

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -31,7 +31,7 @@ export const Spacer = ({ position, size, children }) => {
   return <SpacerView variant={variant}>{children}</SpacerView>;
 };
 
-// Spacer.defaultProps = {
-//   position: "top",
-//   size: "small",
-// };
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
+};
